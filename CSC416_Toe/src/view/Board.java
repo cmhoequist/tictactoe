92e6fb14e6070650e56ff45d0ebd562a660bdaf1
Moritz <Moritz@192.168.3.101>
2016-09-06 21:43:20 -0500

    Move persistence basics

--- CSC416_Toe/src/view/Board.java
+++ CSC416_Toe/src/view/Board.java
@@ -7,6 +7,9 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 import javax.swing.JPanel;
 
@@ -21,6 +24,30 @@ public class Board extends JPanel implements ComponentListener{
 	private int minCoord = 5;
 	private int maxY = 0, maxX = 0;
 	private int height = 0, width = 0;
+	private boolean repaintAll = true;
+	private boolean paintMove = false;
+	private int clickedXIndex = 0, clickedYIndex = 0;
+	private int cellWidth = 0, cellHeight = 0;
+	
+	//Moves
+	private List<Move> moveList = new LinkedList<>();
+	private class Move{
+		private int xIndex = 0, yIndex = 0;
+		boolean isX = true;
+		
+		public Move(int x, int y){
+			xIndex = x; 
+			yIndex = y;
+		}
+		
+		public int getX(){
+			return xIndex;
+		}
+		
+		public int getY(){
+			return yIndex;
+		}
+	}
 	
 	public Board(int rows, int columns){
 		this.rows = rows;
@@ -34,34 +61,78 @@ public class Board extends JPanel implements ComponentListener{
 	
 	@Override
 	public void paintComponent(Graphics g){
-		int yoffset = height/rows;
-		int xoffset = width/columns;
-		for(int i = 0; i < rows; i++){
-			g.drawLine(minCoord, yoffset, maxX, yoffset);	//Draw horizontal lines
-			g.drawLine(xoffset, minCoord, xoffset, maxY);	//Draw vertical lines
-			yoffset = height/rows*(i+1);
-			xoffset = width/columns*(i+1);
-			g.setColor(Color.BLUE);
-			g.drawRect(minCoord, minCoord, width, height);
-			g.setColor(Color.BLACK);
+		//Redraw grid
+		if(repaintAll){	
+			int yoffset = cellHeight;
+			int xoffset = cellWidth;
+			for(int i = 0; i < rows; i++){
+				g.drawLine(minCoord, yoffset, maxX, yoffset);	//Draw horizontal lines
+				g.drawLine(xoffset, minCoord, xoffset, maxY);	//Draw vertical lines
+				yoffset = cellHeight*(i+1);
+				xoffset = cellWidth*(i+1);
+				g.setColor(Color.BLUE);
+				g.drawRect(minCoord, minCoord, width, height);
+				g.setColor(Color.BLACK);
+			}
+			for(Move move : moveList){				//TODO: note that if we somehow have paintMove = true, this will cause problems in a second
+				clickedXIndex = move.getX();
+				clickedYIndex = move.getY();
+				paintMove(g);
+			}
 		}
+		//Paint x in a cell
+		if(paintMove){
+			paintMove(g);
+		}
+
+		//Default behavior 
+		repaintAll = true;
+		paintMove = false;
 	}
 
+	public void paintMove(Graphics g){
+		g.setColor(Color.BLUE);
+		int baseX = clickedXIndex*cellWidth;
+		int baseY = clickedYIndex*cellHeight;
+		g.drawLine(minCoord + baseX + 5, minCoord + baseY + 5, baseX + cellWidth - 5, baseY + cellHeight - 5);
+		g.drawLine(minCoord + baseX + 5, baseY + cellHeight - 5, baseX + cellWidth - 5,  minCoord + baseY + 5);
+	}
+	
 	public void setSizeConstraints(){
 		maxY = getHeight() - exteriorPadding;
 		maxX = getWidth() - exteriorPadding;
 		height = maxY - minCoord;
 		width = maxX - minCoord;
+		cellHeight = height/rows;
+		cellWidth = width/columns;
 	}
 	
-	public int getRow(){
-		//TODO
-		return 0;
+	public void setClickedRow(int x){
+		if(x > width/3 + minCoord){
+			if(x > 2*width/3 + minCoord){
+				clickedXIndex = 2;
+			}
+			else{
+				clickedXIndex = 1;
+			}
+		}
+		else{
+			clickedXIndex = 0;
+		}
 	}
 	
-	public int getColumn(){
-		//TODO
-		return 0;
+	public void setClickedColumn(int y){
+		if(y > height/3 + minCoord){
+			if(y > 2*height/3 + minCoord){
+				clickedYIndex = 2;
+			}
+			else{
+				clickedYIndex = 1;
+			}
+		}
+		else{
+			clickedYIndex = 0;
+		}
 	}
 	
 	//LISTENERS----------------------------------------------------------------------------------------------------------------
@@ -69,8 +140,15 @@ public class Board extends JPanel implements ComponentListener{
 		this.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent mouseEvent){
-				
-			}
+				int x = mouseEvent.getX();
+				int y = mouseEvent.getY();
+				setClickedRow(x);
+				setClickedColumn(y);
+				moveList.add(new Move(clickedXIndex, clickedYIndex));
+				repaintAll = false;
+				paintMove = true;
+				repaint();
+			}	
 		});
 	}
 	
@@ -87,9 +165,4 @@ public class Board extends JPanel implements ComponentListener{
 	@Override
 	public void componentShown(ComponentEvent arg0) {
 	}
-
-
-	
-	
-	
 }
