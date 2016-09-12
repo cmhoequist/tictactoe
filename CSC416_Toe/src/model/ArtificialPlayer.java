b4cf98af1da20d42cff7f718b3a415beb9b6f7a4
cmhoequist <cmhoequist@gmail.com>
2016-09-12 11:32:12 -0500

    AI Update
    *AI now correctly prioritizes making winning moves over blocking losing
    moves

--- CSC416_Toe/src/model/ArtificialPlayer.java
+++ CSC416_Toe/src/model/ArtificialPlayer.java
@@ -16,8 +16,21 @@ public class ArtificialPlayer {
 	
 	public int determineMove(){
 		willWin();
-		//TODO: algorithm
-		return 0;
+		if(winningMove < 0){
+			if(losingMove < 0){
+				for(int i = 0; i < 9; i++){	
+					if(model.checkValid(i)){
+						System.out.println("MOVE (nonterminating): " + i);
+						return i;
+					}
+				}
+				return -1; //if there are no valid moves and we're still asked to determine move, an error has occurred
+			}
+			System.out.println("MOVE:" + losingMove);	
+			return losingMove;
+		}
+		System.out.println("MOVE:" + winningMove);
+		return winningMove;
 	}
 	
 	public boolean willWin(){
@@ -33,26 +46,35 @@ public class ArtificialPlayer {
 				for(int j = 0; j < 3; j++){
 					if(model.getElement(i*3 + j) == null){
 						move = i*3+j;
-						return willWinHelper(rowSum, move);
+						if(willWinHelper(rowSum, move)){	//Only return if it's a win; otherwise keep looking
+							return true;
+						}
+						break;
 					}
 				}
 			}
-			else if(Math.abs(colSum) == 2){
+			if(Math.abs(colSum) == 2){
 				int move = -1;
 				for(int j = 0; j < 3; j++){
 					if(model.getElement(j*3 + i) == null){
 						move = j*3+i;
-						return willWinHelper(colSum, move);
+						if(willWinHelper(colSum, move)){
+							return true;
+						}
+						break;
 					}
 				}
 			}
-			else if(Math.abs(diSum) == 2){
+			if(Math.abs(diSum) == 2){
 				int move = -1;
 				if(i==0){									//Check descending diagonal
 					for(int j = 0; j < 3; j++){
 						if(model.getElement(j*3 + j) == null){
 							move = j*3+j;
-							return willWinHelper(diSum, move);
+							if(willWinHelper(diSum, move)){
+								return true;
+							}
+							break;
 						}
 					}
 				}
@@ -60,34 +82,40 @@ public class ArtificialPlayer {
 					for(int j = 0; j < 3; j++){
 						if(model.getElement((j+1)*2) == null){
 							move = (j+1)*2;
-							return willWinHelper(diSum, move);
+							if(willWinHelper(diSum, move)){
+								return true;
+							}
+							break;
 						}
 					}
 				}
 			}
 		}
+		validateCalculatedMoves();
 		return false;
 	}
 	
 	
 	private boolean willWinHelper(int sum, int move){
+		boolean wins = false;
 		if(sum < 0 && polarity < 0 || sum > 0 && polarity > 0){
 			winningMove = move;
+			wins = true;
 		}
 		else{
 			losingMove = move;
 		}
-		if(model.getElement(move)==null){
-			
-		}
-		
+		validateCalculatedMoves();
+		System.out.println((sum < 0 && polarity < 0 ? "X: " : "O: ") + "w,l: " + winningMove + ", " + losingMove);
+		return wins;
+	}
+	
+	private void validateCalculatedMoves(){
 		if(!model.checkValid(winningMove)){
 			winningMove = -1;
 		}
 		if(!model.checkValid(losingMove)){
 			losingMove = -1;
 		}
-		System.out.println((sum < 0 && polarity < 0 ? "X: " : "O: ") + "w,l: " + winningMove + ", " + losingMove);
-		return true;
 	}
 }
